1d412e4a3cf8fd4a0b799489bd30c0e203437187
Sushil Kumar <ray-ez@RAY-EZs-MAC.local>
2021-10-22 12:49:11 +0530

    15. Connecting nats into Node.js, moved event logic to common, publisher and listener logic are(will be) in each services

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,6 +1,6 @@
-import nats, { Message, Stan } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
-
+import { TicketCreatedListener } from './events/ticket-created-listenter'
 
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -13,20 +13,10 @@ stan.on('connect',()=>{
     console.log('Nats connection Closed!');
     process.exit();
   })
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-  
-  const subscription = stan.subscribe('ticket:created','orders-service-queue-group',options);
 
-  subscription.on('message', (msg: Message)=>{
-    const data = msg.getData();
-    
-    if(typeof data === 'string'){
-      console.log(`Event no(#${msg.getSequence()}) --- Data (${data})`)
-    }
-    msg.ack()
-    console.log('Message recieved');
-  })
+  new TicketCreatedListener(stan).listen();
 })
 
 process.on('SIGINT', ()=>stan.close())
 process.on('SIGTERM', ()=>stan.close())
+
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,33 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
 
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222'
 })
 
-stan.on('connect', ()=>{
-  console.log('Publisher connected to NATS')
+stan.on('connect', async ()=>{
+  console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'one-piece',
-    price: 30
-  })
+  const publisher = new TicketCreatedPublisher(stan);
 
-  stan.publish('ticket:created', data, ()=>{
-    console.log('Event Created');
+  try{
+    await publisher.publish({
+      id: '18',
+      title: 'one-piece',
+      price: 30
   })
+  } catch(err){
+    console.error(err);
+  }
+ 
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'one-piece',
+  //   price: 30
+  // })
+
+  // stan.publish('ticket:created', data, ()=>{
+  //   console.log('Event Created');
+  // })
 }) 
